63a0fae52d2c2083d8551cf99712c7e2ec2d292d
Shakhzod <ShakhzodTurabidinov@gmail.com>
2023-03-22 22:58:44 +0500

    JS qogani

--- main.js
+++ main.js
@@ -56,3 +56,138 @@ bt5.addEventListener("click", () => {
         });
     });
 })
+
+bt10.addEventListener("click", () => {
+    hisob.innerText = 10;
+    lov.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateLover(10)
+        });
+    });
+})
+
+bt15.addEventListener("click", () => {
+    hisob.innerText = 15;
+    lov.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateLover(15)
+        });
+    });
+})
+
+bt20.addEventListener("click", () => {
+    hisob.innerText = 20;
+    lov.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateLover(20)
+        });
+    });
+})
+
+bt5.addEventListener("click", () => {
+    hisob.innerText = 5;
+    upp.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateUP(5)
+        });
+    });
+})
+
+bt10.addEventListener("click", () => {
+    hisob.innerText = 10;
+    upp.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateUP(10)
+        });
+    });
+})
+
+bt15.addEventListener("click", () => {
+    hisob.innerText = 15;
+    upp.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateUP(15)
+        });
+    });
+})
+
+bt20.addEventListener("click", () => {
+    hisob.innerText = 20;
+    upp.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateUP(20)
+        });
+    });
+})
+
+bt5.addEventListener("click", () => {
+    hisob.innerText = 5;
+    num.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateNUM(5)
+        });
+    });
+})
+
+bt10.addEventListener("click", () => {
+    hisob.innerText = 10;
+    num.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateNUM(10)
+        });
+    });
+})
+
+bt15.addEventListener("click", () => {
+    hisob.innerText = 15;
+    num.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateNUM(15)
+        });
+    });
+})
+
+bt20.addEventListener("click", () => {
+    hisob.innerText = 20;
+    num.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateNUM(20)
+        });
+    });
+})
+
+bt5.addEventListener("click", () => {
+    hisob.innerText = 5;
+    sym.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateSYM(5)
+        });
+    });
+})
+
+bt10.addEventListener("click", () => {
+    hisob.innerText = 10;
+    sym.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateSYM(10)
+        });
+    });
+})
+
+bt15.addEventListener("click", () => {
+    hisob.innerText = 15;
+    sym.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateSYM(15)
+        });
+    });
+})
+
+bt20.addEventListener("click", () => {
+    hisob.innerText = 20;
+    sym.addEventListener("click", () => {
+        gg.addEventListener("click", () => {
+            h1.innerText = generateSYM(20)
+        });
+    });
+})
