15f8ec62b9e1c7465a264af11c431e03e2ae9aac
Nurbek <122263233+TongatarovNurbek@users.noreply.github.com>
2023-03-23 11:16:17 +0300

    JS

--- main.js
+++ main.js
@@ -1,195 +1,129 @@
-const bt5 = document.querySelector(".btn5");
-const bt10 = document.querySelector(".btn10");
-const bt15 = document.querySelector(".btn15");
-const bt20 = document.querySelector(".btn20");
-const hisob = document.querySelector(".hisob");
-const lov = document.querySelector(".LOV");
-const upp = document.querySelector(".UPP");
-const num = document.querySelector(".NUM");
-const sym = document.querySelector(".SYM");
-const gg = document.querySelector(".gg");
-const h1 = document.querySelector("h2");
 
-function generateLover(son){
-    let abs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
-    let gen =  Math.floor(Math.random() * abs.length)
-    if(son == 5) sum =  abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    if(son == 10) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 15) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 20) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    return sum
+console.log("Password Generator");
+
+
+const btn5 = document.getElementById("btn5")
+const btn10 = document.getElementById("btn10")
+const btn15 = document.getElementById("btn15")
+const btn20 = document.getElementById("btn20")
+
+const btngetter = document.getElementById("gg")
+const inputText = document.getElementById("inputText")
+
+const inNum = document.getElementById("NUM")
+const inUpper = document.getElementById("UPP")
+const inLower = document.getElementById("LOV")
+const inSymbol = document.getElementById("SYM")
+
+let random
+
+let count = true
+let btnNum = false
+let upperC = false
+let lowerC = false
+let symbolC = false
+let yig = ""
+
+
+inSymbol.addEventListener("click", function() {
+    if (symbolC) symbolC = false
+    else symbolC = true
+})
+
+inNum.addEventListener("click", function() {
+    if (btnNum) btnNum = false
+    else btnNum = true
+})
+
+inUpper.addEventListener("click", function() {
+    if (upperC) upperC = false
+    else upperC = true
+})
+
+inLower.addEventListener("click", function() {
+    if (lowerC) lowerC = false
+    else lowerC = true
+})
+
+
+
+btn5.addEventListener("click", function () {
+    btnGetter(btn5)
+})
+
+btn10.addEventListener("click", function () {
+    btnGetter(btn10)
+})
+btn15.addEventListener("click", function () {
+    btnGetter(btn15)
+})
+btn20.addEventListener("click", function () {
+    btnGetter(btn20)
+})
+
+
+
+function upper(btn) {
+    if (upperC) {
+        for (let i = 0; btn > i; i++) {
+            random = Math.floor(Math.random() * 26)
+            let harfK = "QWERTYUIOPLKJHGFDSAZXCVBNM"
+            yig += harfK[random]
+        }
+        console.log(yig);
+    }
+    return yig
 }
-function generateUP(son){
-    let abs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
-    let gen =  Math.floor(Math.random() * abs.length)
-    let sum = abs[gen] 
-    if(son == 5) sum =  abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    if(son == 10) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 15) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 20) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    return sum
-}
-function generateSYM(son){
-    let abs = ["A", "/", "*", "/", "-", "o", "+", "H", "i", "j", "K", "L"]
-    let gen =  Math.floor(Math.random() * abs.length)
-    let sum = abs[gen] 
-    if(son == 5) sum =  abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    if(son == 10) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 15) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 20) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    return sum
-}
-function generateNUM(son){
-    let gen =  Math.floor(Math.random() * 99)
-    if(son == 5) sum =  abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    if(son == 10) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 15) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen];
-    if(son == 20) sum = abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen] + abs[gen]
-    return sum
+
+function lower(btn) {
+    if (lowerC) {
+        for (let i = 0; btn > i; i++) {
+            random = Math.floor(Math.random() * 26)
+            let kichik = "qwertyuioplkjhgfdsazxcvbnm"
+            yig += kichik[random]
+        }
+        console.log(yig);
+    }
+    return yig
 }
 
-bt5.addEventListener("click", () => {
-    hisob.innerText = 5;
-    lov.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateLover(5)
-        });
-    });
-})
 
-bt10.addEventListener("click", () => {
-    hisob.innerText = 10;
-    lov.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateLover(10)
-        });
-    });
-})
 
-bt15.addEventListener("click", () => {
-    hisob.innerText = 15;
-    lov.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateLover(15)
-        });
-    });
-})
+function randomNum(btn) {
+    if (btnNum) {
+        for (let i = 0; btn > i; i++) {
+            random = Math.floor(Math.random() * 9)
+            let num = "1234567890"
+            yig = yig + "" + num[random]
+        }
+        console.log(yig);
+    }
+    return yig
+}
 
-bt20.addEventListener("click", () => {
-    hisob.innerText = 20;
-    lov.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateLover(20)
-        });
-    });
-})
+function Symbol(btn) {
+    if (symbolC) {
+        for (let i = 0; btn > i; i++) {
+            random = Math.floor(Math.random() * 13)
+            let num = "!@#$%^&*(*)/-+"
+            yig = yig + "" + num[random]
+        }
+        console.log(yig);
+    }
+    return yig
+}
 
-bt5.addEventListener("click", () => {
-    hisob.innerText = 5;
-    upp.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateUP(5)
-        });
-    });
-})
 
-bt10.addEventListener("click", () => {
-    hisob.innerText = 10;
-    upp.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateUP(10)
-        });
-    });
-})
+function btnGetter (btn) {
+    btn.style.backgroundColor = "rgb(115, 236, 115)"
+    btngetter.addEventListener("click",function() {
+        btn.style.backgroundColor = "blueviolet"
 
-bt15.addEventListener("click", () => {
-    hisob.innerText = 15;
-    upp.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateUP(15)
-        });
-    });
-})
-
-bt20.addEventListener("click", () => {
-    hisob.innerText = 20;
-    upp.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateUP(20)
-        });
-    });
-})
-
-bt5.addEventListener("click", () => {
-    hisob.innerText = 5;
-    num.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateNUM(5)
-        });
-    });
-})
-
-bt10.addEventListener("click", () => {
-    hisob.innerText = 10;
-    num.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateNUM(10)
-        });
-    });
-})
-
-bt15.addEventListener("click", () => {
-    hisob.innerText = 15;
-    num.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateNUM(15)
-        });
-    });
-})
-
-bt20.addEventListener("click", () => {
-    hisob.innerText = 20;
-    num.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateNUM(20)
-        });
-    });
-})
-
-bt5.addEventListener("click", () => {
-    hisob.innerText = 5;
-    sym.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateSYM(5)
-        });
-    });
-})
-
-bt10.addEventListener("click", () => {
-    hisob.innerText = 10;
-    sym.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateSYM(10)
-        });
-    });
-})
-
-bt15.addEventListener("click", () => {
-    hisob.innerText = 15;
-    sym.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateSYM(15)
-        });
-    });
-})
-
-bt20.addEventListener("click", () => {
-    hisob.innerText = 20;
-    sym.addEventListener("click", () => {
-        gg.addEventListener("click", () => {
-            h1.innerText = generateSYM(20)
-        });
-    });
-})
-
-console.log("Salom");
+        if (btnNum) inputText.value = randomNum(btn.innerText)
+        if (upperC) inputText.value = upper(btn.innerText)
+        if (lowerC) inputText.value = lower(btn.innerText)
+        if (symbolC) inputText.value = Symbol(btn.innerText)
+        inputText.value = yig
+        yig = ""
+    })
+}
